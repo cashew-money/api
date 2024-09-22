1b0398ae763be565a562cc6711037fda5b949ffa
William Zheng <williamzheng2001@gmail.com>
2024-09-22 15:12:40 -0400

    send json response

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
@@ -9,6 +8,14 @@ import (
 
 func Healthcheck(app *Application) httprouter.Handle {
 	return func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-		fmt.Fprintln(w, "status: available")
+		data := map[string]string{
+			"status": "available",
+		}
+
+		err := app.writeJSON(w, http.StatusOK, data, nil)
+		if err != nil {
+			app.Logger.Error(err.Error())
+			http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+		}
 	}
 }
