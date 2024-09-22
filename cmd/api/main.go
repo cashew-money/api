66145e6b6749830068f8197a9f2c8ede261048df
William Zheng <williamzheng2001@gmail.com>
2024-09-22 14:12:51 -0400

    connect to db

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"log/slog"
 	"os"
+	"time"
+
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 type Config struct {
 	Port int
+	DB   struct {
+		DSN string
+	}
 }
 
 type Application struct {
@@ -19,18 +26,47 @@ func main() {
 	var cfg Config
 
 	flag.IntVar(&cfg.Port, "port", 4000, "API server port")
+	flag.StringVar(&cfg.DB.DSN, "db-dsn", "", "PostgreSQL DSN")
+
 	flag.Parse()
 
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	db, err := openDB(cfg.DB.DSN)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+	defer db.Close()
+
+	logger.Info("database connection pool established")
 
 	app := &Application{
 		Config: cfg,
 		Logger: logger,
 	}
 
-	err := app.Serve()
+	err = app.Serve()
 	if err != nil {
 		logger.Error(err.Error())
 		os.Exit(1)
 	}
+}
+
+func openDB(dsn string) (*pgxpool.Pool, error) {
+	pool, err := pgxpool.New(context.Background(), dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err = pool.Ping(ctx)
+	if err != nil {
+		pool.Close()
+		return nil, err
+	}
+
+	return pool, nil
 }
